ce32e5a33db86b99596d0d6879b18d91e9c85628
Muhammad Hamza <hamzaadil56@gmail.com>
2024-10-10 21:57:55 +0500

    added project's config

---
+++ researchmind/main.py
@@ -0,0 +1,54 @@
+from openai import OpenAI
+
+base_url = "https://api.aimlapi.com/v1"
+api_key = "842b61037d4049a3b3f824ffdba905b3"
+system_prompt = "You are a travel agent. Be descriptive and helpful."
+user_prompt = "Tell me about San Francisco"
+
+client = OpenAI(api_key=api_key, base_url=base_url)
+
+
+prompt = """
+Instructions:
+- Given the React component below, change it so that nonfiction books have red
+  text. 
+- Return only the code in your reply
+- Do not include any additional formatting, such as markdown code blocks
+- For formatting, use four space tabs, and do not allow any lines of code to 
+  exceed 80 columns
+
+const books = [
+  { title: 'Dune', category: 'fiction', id: 1 },
+  { title: 'Frankenstein', category: 'fiction', id: 2 },
+  { title: 'Moneyball', category: 'nonfiction', id: 3 },
+];
+
+export default function BookList() {
+  const listItems = books.map(book =>
+    <li>
+      {book.title}
+    </li>
+  );
+
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+"""
+
+response = client.chat.completions.create(
+    model="o1-mini",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": prompt
+                },
+            ],
+        }
+    ]
+)
+
+print(response.choices[0].message.content)
