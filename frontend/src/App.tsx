2dd81c88d99668ab75ef87ab9d8285217c09dded
Muhammad Hamza <hamzaadil56@gmail.com>
2024-10-11 20:49:06 +0500

    fixed ts error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
-
 import "./App.css";
 import ResearchAIDashboard from "./components/ResearchDashboard";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <ResearchAIDashboard />
